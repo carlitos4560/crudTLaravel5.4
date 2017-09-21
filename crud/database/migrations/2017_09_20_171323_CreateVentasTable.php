e3b046c092c2d3011cea600ae11fe8a1cb37dab4
carlitos4560 <carlitosrobertoes@gmail.com>
2017-09-21 05:19:40 -0400

    confirmando

---
+++ crud/database/migrations/2017_09_20_171323_CreateVentasTable.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('fecha');
+            $table->string('descripcion');
+            
+            $table->integer('id_cliente')->unsigned();
+            $table->foreign('id_cliente')->references('id')->on('clientes');
+            
+            $table->integer('id_producto')->unsigned();
+            $table->foreign('id_producto')->references('id')->on('productos');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
